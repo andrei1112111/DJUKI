d13e313140ad5f6cf64c57f955a17103564b98f0
andrei1112111 <atishkin060705@gmail.com>
2023-02-19 22:26:32 +0700

    server funk

--- client.py
+++ client.py
@@ -1,107 +1,28 @@
-import pygame
-import sys
-import os
+import requests
+
+server_path = "http://127.0.0.1:5000"
 
 
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    # если файл не существует, то выходим
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
+def get_management_server(current=''):
+    """get_management_server('ochered') ---> {'game': True}"""
+    """get_management_server() ---> {'govno': [13, 13], 'juck1': [12, 113], 'juck2': [122, 113]}"""
+    if current == 'ochered':
+        return requests.get(f'{server_path}/och/').json()
     else:
-        image = image.convert_alpha()
-    return image
+        return requests.get(f'{server_path}').json()
 
 
-FPS = 50
-pygame.init()
-size = width, height = 1920, 1000
-
-bug_sprite = pygame.sprite.Group()
-
-display = pygame.display
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption('Навозники')
-clock = pygame.time.Clock()
-
-bug_image = load_image('bug.png')
-tile_width = tile_height = 50
-
-
-class Bug(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y):
-        super().__init__(bug_sprite)
-        self.image = bug_image
-        self.rect = self.image.get_rect().move(0, 0)
-        self.pos = pos_x, pos_y
-
-    def move(self, x, y):
-        pass
-
-
-class Board:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.board = [[0] * width for _ in range(height)]
-        self.left = 10
-        self.top = 10
-        self.cell_size = 30
-
-    def set_view(self, left, top, cell_size):
-        self.left = left
-        self.top = top
-        self.cell_size = cell_size
-
-    def get_cell(self, mouse_pos):
-        if not (0 <= mouse_pos[0] - self.left <= self.width * self.cell_size and 0 <= mouse_pos[
-            1] - self.top <= self.height * self.cell_size):
-            return None
-        i = mouse_pos[1] - self.top
-        j = mouse_pos[0] - self.left
-        i = i // self.cell_size
-        j = j // self.cell_size
-        return (j, i)
-
-    def get_click(self, mouse_pos):
-        cell = self.get_cell(mouse_pos)
-        self.on_click(cell)
-
-
-def terminate():
-    pygame.quit()
-    sys.exit()
-
-
-'''def get_management_server() -> dict:
-    """dict:
-            ОЧЕРЕДЬ:
-                    [Начало игры / не начало игры(bool: True - начало игры, False - еще ждем)]
-            ИГРА:
-                [[ПОложение врежеского жука X(int), ПОложение врежеского жука Y(int)], [ПОложение говна X(int), ПОложение говна Y(int)], Игра закончилась?(bool: False - играем, True - конец игры)]
-    """
-    # ОЧЕРЕДЬ return [True]
-    # ИГРА return [[12, 123], [123, 123], False]
+def send_data(game_data, current=''):
+    """send_data({}, 'ochered') ---> {'game': False, 'you': 'juck1'}  ||  {'game': True}"""
+    """send_data({'name': 'juck1', 'my': [12, 113], 'govno': [13, 13]}) ---> {'all': 'ok'}"""
+    if current == 'ochered':
+        return requests.post(f'{server_path}/och/', json=game_data).json()
+    else:
+        return requests.post(f'{server_path}', json=game_data).json()
     pass
 
 
-def send_data(game_data: dict[[int, int], [int, int]]):
-    """game_data:
-                [[ПОложение жука X(int), ПОложение жука Y(int)], [ПОложение говна X(int), ПОложение говна Y(int)]]"""
-    pass
-'''
 # КОД ИГРЫ -->
+
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-    screen.fill(pygame.Color(22, 74, 27))
-    bug_sprite.draw(screen)
-    display.flip()
+    exec(f"print({input('-> ')})")
--- server.py
+++ server.py
@@ -1,1 +1,41 @@
-print("hello")
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+game_data = {"juck1": [], "juck2": [], "govno": []}
+ochered = 0
+
+
+@app.route('/och/', methods=['GET', 'POST'])
+def och():
+    global ochered
+    global game_data
+    if request.method == 'GET':
+        if ochered == 2:
+            return jsonify({"game": True})
+        else:
+            return jsonify({"game": False})
+    elif request.method == 'POST':
+        if ochered == 1:
+            ochered += 1
+            return jsonify({"game": True, "you": "juck2"})
+        else:
+            ochered += 1
+            return jsonify({"game": False, "you": "juck1"})
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    global game_data
+    if request.method == 'GET':
+        return jsonify(game_data)
+    elif request.method == 'POST':
+        data = request.json
+        print(data)
+        game_data[data["name"]] = data["my"]
+        if data["name"] == 'juck1':
+            game_data["govno"] = data["govno"]
+    return jsonify({"all": 'ok'})
+
+
+app.run(debug=True)
